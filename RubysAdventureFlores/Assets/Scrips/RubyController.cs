025888abf43c3a8776f88eefcdd6ccd703ecee45
Andrewfro <743961@student.dusd.net>
2022-12-02 09:57:01 -0800

    Health Script
    Added collectable healths scrip and also adjusted ruby controller scrip

--- RubysAdventureFlores/Assets/Scrips/RubyController.cs
+++ RubysAdventureFlores/Assets/Scrips/RubyController.cs
@@ -7,6 +7,13 @@ using UnityEngine.UIElements;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -15,6 +22,8 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -27,15 +36,21 @@ public class RubyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        
-    //Create our movenment vector
+
+        //Create our movenment vector
         Vector2 position = transform.position;
 
         //Create horizontol and verticol movement
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //setting the new position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
+
